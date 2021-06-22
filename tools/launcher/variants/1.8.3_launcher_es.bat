7f6f2942e9e0ef88830fe0612dc543f0cc406112
NGDPL Nk <nikkotilion@gmail.com>
2021-06-22 18:46:09 -0400

    Actualización 1.8.3_launcher_es.bat
    Se actualiza el código para la herramienta Lanzador de la variante de la versión 1.8.3.

--- tools/launcher/variants/1.8.3_launcher_es.bat
+++ tools/launcher/variants/1.8.3_launcher_es.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBVdRw+NAE+1BaAR7ebv/Nagq1k1QeADdYzIyaCBL+wA1lHrepo+6nZbjcUPBB5KQh6qewE3oWtQiW2GOMnSugzuKg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksaHUrSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlTMbSXqZg==
+::ZQ05rAF9IAHYFVzEqQIdCyhnYAWXMmiyA6co5+f3r+yIsA1MGrpxK8/o36aYJfIAig==
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmhD1iWB6AS6+D+/d6IrUFdUu0tON6VgvrdYNMW+FPhZ4VN
+::fBEirQZwNQPfEVWB+kM9LVsJDCmhD1iWB6AS6+D+/d6IrUFdUu0tON6VgvrdYNMW+FPhZ4VN
+::cRolqwZ3JBvQF1fEqQIdCyhnYAWXMmiyA6co5+f3r+yIsA1MGrpxK8/o36aYJfIAig==
+::dhA7uBVwLU+EWHej0HIcaDVfLA==
+::YQ03rBFzNR3SWATE2mUUGDcUYg/i
+::dhAmsQZ3MwfNWATEVotieEkFDCqlGFuWRpsXiA==
+::ZQ0/vhVqMQ3MEVWAtB9wBjxwfCjCEmDa
+::Zg8zqx1/OA3MEVWAtB9wBjxwfCjCEmDa
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBVdRw+NAE+/Fb4I5/jHw+uSrE4VUfADYNmP5piPLvoS7kr2NZIolklfit0FCBRKewHrP0Fs5z0CrjLRecKEtm8=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
 :init
