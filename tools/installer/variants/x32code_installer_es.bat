a8d8bbba60db7d394f5dc1a56635b83bb896fb98
NGDPL Nk <nikkotilion@gmail.com>
2021-06-22 23:28:37 -0400

    Actualización x32code_installer_es.bat
    Se actualiza el código para la herramienta Instalador de la variante x32

--- tools/installer/variants/x32code_installer_es.bat
+++ tools/installer/variants/x32code_installer_es.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBVdRw+NAE+1BaAR7ebv/Nagq1k1QeADdYzIyaCBL+wA1lHrepo+6nNUi98NABddbC6iYBwgqGJOs3a6PsSTvUHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZksaHUrSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlTMbSXqZg==
+::ZQ05rAF9IAHYFVzEqQIdCyhnZQqRKGq2CrAO3Of044o=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmhD1iTCKYI6eT36vizrUIRNA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCmhD1iTCKYI6eT36vizrUIRNA==
+::cRolqwZ3JBvQF1fEqQIdCyhnZQqRKGq2CrAO3Of044o=
+::dhA7uBVwLU+EWHej0HIcaDVfLA==
+::YQ03rBFzNR3SWATE2mUUGDcUYg/i
+::dhAmsQZ3MwfNWATEVotieEkFDCqlGFuWRpsXiA==
+::ZQ0/vhVqMQ3MEVWAtB9wBjxwfCjCEmDa
+::Zg8zqx1/OA3MEVWAtB9wBjxwfCjCEmDa
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBVdRw+NAE+/Fb4I5/jHxuSUtkwRVeYwas/f3/S9JfIF40HrZ5M+lmIJyoUJFB44
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
 :init
