6210e4ea5ce7376e2b916c4486d84c42be16df19
NGDPL Nk <nikkotilion@gmail.com>
2021-09-13 11:44:44 -0300

    Reorganización de directorios

---
+++ var/linux-deb/launcher/maincode_launcher_beta_es.sh
@@ -0,0 +1,8 @@
+echo Lanzador de Servidores de Minecraft
+echo MCSSTools for Linux Beta por NGDPL Nk
+echo --------------------------------------
+echo 
+echo Inciando el Server con 3GB de Ram...
+echo 
+java -Xmx3G -Xms3G -jar Server_1.17.1.jar -nogui
+
