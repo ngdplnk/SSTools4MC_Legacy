582d6d7711a33f7613fab03281ed357a7c0ed6db
Nicolás Valdés <nikkotilion@gmail.com>
2023-08-30 11:03:15 -0400

    Actualización maincode_launcher_beta_es.sh
    Se actualiza mínimamente el código solo para comodidad del desarrollador. No existen planes cercanos de seguir con el desarrollo de la herramienta en Linux.

--- var/linux-deb/launcher/maincode_launcher_beta_es.sh
+++ var/linux-deb/launcher/maincode_launcher_beta_es.sh
@@ -1,6 +1,6 @@
 echo Lanzador de Servidores para Minecraft
 echo --------------------------------------
 echo 
-echo Inciando el Server con 3GB de Ram...
+echo Inciando el Server con 4GB de Ram...
 echo 
-java -Xmx3G -Xms3G -jar Server_1.17.1.jar -nogui
+java -Xmx4G -Xms4G -jar server.jar -nogui
