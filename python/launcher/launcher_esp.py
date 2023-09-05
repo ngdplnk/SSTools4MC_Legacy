94ff5c6476431b58ec8c9790c85d53b3b0d95c34
Nicolás Valdés <nikkotilion@gmail.com>
2023-09-05 00:28:40 -0300

    Update launcher_esp.py

--- python/launcher/launcher_esp.py
+++ python/launcher/launcher_esp.py
@@ -20,6 +20,10 @@ if sys.platform.startswith('win32'):#WINDOWS
 elif sys.platform.startswith('linux') or sys.platform.startswith('darwin'):#LINUX O MACOS
     sys.stdout.write(f"\x1b]2;Lanzador de Servidores para Minecraft\x07")
 
+#REGRESO
+def regreso():
+    return
+
 #CONFIGURACIÓN
 def config():
     limpiar_consola()
@@ -31,10 +35,15 @@ def config():
         hc = None
         hci = None
         hcf = None
+        pvp = None
+        pvpi = None
+        pvpf = None
+        global modo
         modo = None
+        global dificultad
         dificultad = None
         cantprops = None
-        linea_29 = "."
+        linea_30 = "."
         with open(props, "r") as lectura:
             lineas = lectura.readlines()
         if len(lineas) >= 7:
@@ -43,48 +52,52 @@ def config():
                 lineas[6] = ""
             elif "debug=true" in linea_7_1:
                 lineas[6] = ""
+        if len(lineas) >= 23:
+            linea_23 = lineas[22]
+            if "hellworld=false" in linea_23:
+                lineas[22] = ""
+            elif "hellworld=true" in linea_23:
+                lineas[22] = ""
+        if len(lineas) >= 41:
+            linea_41 = lineas[40]
+            if "previews-chat=false" in linea_41:
+                lineas[40] = ""
+            elif "previews-chat=true" in linea_41:
+                lineas[40] = ""
+        if len(lineas) >= 54:
+            linea_54 = lineas[53]
+            if "snooper-enabled=false" in linea_54:
+                lineas[53] = ""
+            elif "snooper-enabled=true" in linea_54:
+                lineas[53] = ""
+        if len(lineas) >= 61:
+            linea_61 = lineas[60]
+            if "texture-pack=" in linea_61:
+                lineas[60] = ""
             with open(props, "w") as propsupd:
                 propsupd.writelines(lineas)
-        if len(lineas) >= 21:
-            linea_21_1 = lineas[20]
-            if "hellworld=false" in linea_21_1:
-                lineas[20] = ""
-            elif "hellworld=true" in linea_21_1:
-                lineas[20] = ""
-            with open(props, "w") as propsupd:
-                propsupd.writelines(lineas)
-        if len(lineas) >= 38:
-            linea_38 = lineas[37]
-            if "previews-chat=false" in linea_38:
-                lineas[37] = ""
-            elif "previews-chat=true" in linea_38:
-                lineas[37] = ""
-            with open(props, "w") as propsupd:
-                propsupd.writelines(lineas)
-        if len(lineas) >= 51:
-            linea_51 = lineas[50]
-            if "snooper-enabled=false" in linea_51:
-                lineas[50] = ""
-            elif "snooper-enabled=true" in linea_51:
-                lineas[50] = ""
-            with open(props, "w") as propsupd:
-                propsupd.writelines(lineas)
-        if len(lineas) >= 56:
-            linea_56 = lineas[55]
-            if "texture-pack=" in linea_56:
-                lineas[55] = ""
-            with open(props, "w") as propsupd:
-                propsupd.writelines(lineas)
-        if len(lineas) >= 34:
-            linea_34 = lineas[33]
-            if "online-mode=true" in linea_34:
+        if len(lineas) >= 35:
+            linea_35 = lineas[34]
+            if "online-mode=true" in linea_35:
                 online = False
-            elif "online-mode=false" in linea_34:
+            elif "online-mode=false" in linea_35:
                 online = True
         if online == True:
             onlinetxt = "ONLINE"
         elif online == False:
             onlinetxt = "OFFLINE"
+        if len(lineas) >= 39:
+            linea_39 = lineas[38]
+            if "pvp=true" in linea_39:
+                pvp = False
+            elif "pvp=false" in linea_39:
+                pvp = True
+        if pvp == True:
+            pvpi = "ACTIVAR"
+            pvpf = "PvP ACTIVADO"
+        elif pvp == False:
+            pvpi = "DESACTIVAR"
+            pvpf = "PvP DESACTIVADO"
         if len(lineas) >= 21:
             linea_21 = lineas[20]
             if "hardcore=false" in linea_21:
@@ -133,22 +146,22 @@ def config():
                 dificultad = "DIFÍCIL"
             elif "difficulty=3" in linea_7:
                 dificultad = "DIFÍCIL"
-        if len(lineas) >= 29:
-            linea_29 = lineas[28]
-            cantprops = re.findall(r'\d+', linea_29)
+        if len(lineas) >= 30:
+            linea_30 = lineas[29]
+            cantprops = re.findall(r'\d+', linea_30)
         with open(props, "w") as propsupd:
             propsupd.writelines(lineas)
         limpiar_consola()
         print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nConfiguración del Servidor\n")
         print("(1) Cambiar a Modo",onlinetxt)
-        print("(2)",hci,"Modo Extremo\n(3) Cambiar Modo de Juego\n(4) Cambiar Dificultad\n(5) Cambiar máximo de Jugadores\n(6) Volver al Menú Principal\n")
+        print("(2)",hci,"Modo Extremo\n(3)",pvpi,"PvP\n(4) Cambiar Modo de Juego\n(5) Cambiar Dificultad\n(6) Cambiar máximo de Jugadores\n(7) Volver al Menú Principal\n")
         confsel = input("Selecciona una opción= ")
         if confsel == "1":
             limpiar_consola()
             if online == True:
-                lineas[33] = "online-mode=true\n"
+                lineas[34] = "online-mode=true\n"
             elif online == False:
-                lineas[33] = "online-mode=false\n"
+                lineas[34] = "online-mode=false\n"
             with open(props, "w") as propsupd:
                 propsupd.writelines(lineas)
             print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nModo Cambiado a",onlinetxt)
@@ -168,76 +181,103 @@ def config():
             config()
         elif confsel == "3":
             limpiar_consola()
-            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nEl modo de juego actual es",modo)
-            print("")
-            modosel = input("(1) Supervivencia\n(2) Creativo\n(3) Aventura\n(4) Espectador\n(5) Volver Atrás\n(6) Volver al menú principal\n\nElige un el nuevo Modo de Juego= ")
-            if modosel == "1":
-                lineas[17] = "gamemode=0\n"
-                modo = "SUPERVIVENCIA"
-            elif modosel == "2":
-                lineas[17] = "gamemode=1\n"
-                modo = "CREATIVO"
-            elif modosel == "3":
-                lineas[17] = "gamemode=2\n"
-                modo = "AVENTURA"
-            elif modosel == "4":
-                lineas[17] = "gamemode=3\n"
-                modo = "ESPECTADOR"
-            elif modosel == "6":
-                return
-            else:
-                config()
+            if pvp == True:
+                lineas[38] = "pvp=true\n"
+            elif pvp == False:
+                lineas[38] = "pvp=false\n"
             with open(props, "w") as propsupd:
                 propsupd.writelines(lineas)
-            limpiar_consola()
-            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nCambiado a Modo",modo)
+            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n")
+            print(pvpf)
             time.sleep(1.5)
             config()
         elif confsel == "4":
-            limpiar_consola()
-            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nLa dificultad actual es",dificultad)
-            print("")
-            difsel = input("(1) Pacífico\n(2) Fácil\n(3) Normal\n(4) Difícil\n(5) Volver Atrás\n(6) Volver al menú principal\n\nElige la nueva Dificultad= ")
-            if difsel == "1":
-                lineas[6] = "difficulty=0\n"
-                dificultad = "PACÍFICO"
-            elif difsel == "2":
-                lineas[6] = "difficulty=1\n"
-                dificultad = "FÁCIL"
-            elif difsel == "3":
-                lineas[6] = "difficulty=2\n"
-                dificultad = "NORMAL"
-            elif difsel == "4":
-                lineas[6] = "difficulty=3\n"
-                dificultad = "DIFÍCIL"
-            elif difsel == "6":
-                return
-            else:
-                config()
-            with open(props, "w") as propsupd:
-                propsupd.writelines(lineas)
-            limpiar_consola()
-            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nCambiado a Dificultad",dificultad)
-            time.sleep(1.5)
-            config()
-        elif confsel == "5":
-            limpiar_consola()
-            print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nEl límite actual de Jugadores es",cantprops)
-            print("")
-            cantnueva = input("Escribe el nuevo límite de Jugadores para el Servidor= ")
-            try:
-                entero = int(cantnueva)
-                cantmod = re.sub(r'\d+',str(entero),linea_29)
-                lineas[28] = cantmod
+            def juego():
+                global modo
+                limpiar_consola()
+                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nEl modo de juego actual es",modo)
+                print("")
+                modosel = input("(1) Supervivencia\n(2) Creativo\n(3) Aventura\n(4) Espectador\n(5) Volver Atrás\n(6) Volver al menú principal\n\nElige un el nuevo Modo de Juego= ")
+                if modosel == "1":
+                    lineas[17] = "gamemode=0\n"
+                    modo = "SUPERVIVENCIA"
+                elif modosel == "2":
+                    lineas[17] = "gamemode=1\n"
+                    modo = "CREATIVO"
+                elif modosel == "3":
+                    lineas[17] = "gamemode=2\n"
+                    modo = "AVENTURA"
+                elif modosel == "4":
+                    lineas[17] = "gamemode=3\n"
+                    modo = "ESPECTADOR"
+                elif modosel == "5":
+                    config()
+                elif modosel == "6":
+                    return
+                else:
+                    juego()
                 with open(props, "w") as propsupd:
                     propsupd.writelines(lineas)
                 limpiar_consola()
-                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nLímite de Jugadores Cambiado a",entero)
+                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nCambiado a Modo",modo)
                 time.sleep(1.5)
                 config()
-            except ValueError:
+            juego()
+        elif confsel == "5":
+            def difconf():
+                global dificultad
+                limpiar_consola()
+                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nLa dificultad actual es",dificultad)
+                print("")
+                difsel = input("(1) Pacífico\n(2) Fácil\n(3) Normal\n(4) Difícil\n(5) Volver Atrás\n(6) Volver al menú principal\n\nElige la nueva Dificultad= ")
+                if difsel == "1":
+                    lineas[6] = "difficulty=0\n"
+                    dificultad = "PACÍFICO"
+                elif difsel == "2":
+                    lineas[6] = "difficulty=1\n"
+                    dificultad = "FÁCIL"
+                elif difsel == "3":
+                    lineas[6] = "difficulty=2\n"
+                    dificultad = "NORMAL"
+                elif difsel == "4":
+                    lineas[6] = "difficulty=3\n"
+                    dificultad = "DIFÍCIL"
+                elif difsel == "5":
+                    config()
+                elif difsel == "6":
+                    return
+                else:
+                    difconf()
+                with open(props, "w") as propsupd:
+                    propsupd.writelines(lineas)
+                limpiar_consola()
+                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nCambiado a Dificultad",dificultad)
+                time.sleep(1.5)
                 config()
+            difconf()
         elif confsel == "6":
+            def playerscant():
+                limpiar_consola()
+                print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nEl límite actual de Jugadores es",cantprops)
+                print("")
+                cantnueva = input("Escribe el nuevo límite de Jugadores para el Servidor= ")
+                try:
+                    entero = int(cantnueva)
+                    if entero <= 0 or entero > 1024:
+                        playerscant()
+                    else:
+                        cantmod = re.sub(r'\d+',str(entero),linea_30)
+                        lineas[29] = cantmod
+                        with open(props, "w") as propsupd:
+                            propsupd.writelines(lineas)
+                        limpiar_consola()
+                        print("Lanzador de Servidores para Minecraft\n-------------------------------------\n\nLímite de Jugadores Cambiado a",entero)
+                        time.sleep(1.5)
+                        config()
+                except ValueError:
+                    playerscant()
+            playerscant()
+        elif confsel == "7":
             return
         else:
             config()
