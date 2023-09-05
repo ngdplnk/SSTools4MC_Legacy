e6c37bf55209e3be803d5dbcd1a6f9f57239c575
Nicolás Valdés <nikkotilion@gmail.com>
2023-09-05 10:18:49 -0300

    Añadida carpeta de archivos FPI
    Se añade carpeta para apuntes y archivos de Fundamentos de Programación para Ingeniería.

---
+++ fpi/apuntes.py
@@ -0,0 +1,6 @@
+#5 de Septiembre de 2023
+"\n es para saltos de línea"
+"\t es para tabulaciones"
+#en el caso de querer hacer una suma de números asigandos a variables a partir de la entrada del usuario, se debe usar:
+"a = int(input('texto de la entrada'))"
+#no se recomienda hacer operaciones anidadas ya que si existe un error se puede debuggear de manera más simple (por líneas separadas)
