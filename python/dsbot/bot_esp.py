bf4a55c1f8ed8e0e8480f2a15c7d34dacf78bc6a
NGDPLNk <nikkotilion@gmail.com>
2023-09-12 19:04:55 -0300

    Actualizado bot_esp.py

--- python/dsbot/bot_esp.py
+++ python/dsbot/bot_esp.py
@@ -1,30 +1,57 @@
 #MÓDULOS
 import discord
 from discord.ext import commands
+import asyncio
 
-#DEF INTENCIONES
+#INTENTS
 intents = discord.Intents.default()
-intents.typing = True
+intents.typing = False
 intents.presences = False
 
-#PREFIJO
+#PREFIX
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-#EVENTO INICIO
-@bot.event
-async def on_ready():
-    print(f'Bot conectado como {bot.user.name}')
-
-#COM SERVIDOR ABIERTO
-@bot.command()
-async def servidor_abierto(ctx):
-    try:
-        await ctx.send('El SMP está abierto! Únete')
-    except Exception as e:
-        print(f"Error al ejecutar el comando !servidor_abierto: {e}")
 
 #TOKEN
 TOKEN = ''
 
-#INICIO
-bot.run(TOKEN)
+#ID ESTADO
+canal_id = 
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} iniciado correctamente.')
+    canal = bot.get_channel(canal_id)
+    if canal:
+        await canal.purge()
+        await canal.send(f'El SMP está abierto!\n\nIP\n\n||aqui debería ir la ip:42069||\n\n1.20.1')
+
+@bot.event
+async def on_disconnect():
+    canal = bot.get_channel(canal_id)
+    if canal:
+        await canal.purge()
+        await canal.send('El SMP está cerrado.')
+
+@bot.command()
+async def ip(ctx):
+    canal = ctx.channel
+    await canal.send('La IP del server es')
+
+#ERRORES
+@bot.event
+async def on_error(event, *args, **kwargs):
+    print(f"Error en evento {event}: {args} {kwargs}")
+
+async def main():
+    try:
+        await bot.start(TOKEN)
+    except KeyboardInterrupt:
+        canal = bot.get_channel(canal_id)
+        if canal:
+            await canal.purge()
+            await canal.send('El SMP está cerrado.')
+        await bot.close()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
