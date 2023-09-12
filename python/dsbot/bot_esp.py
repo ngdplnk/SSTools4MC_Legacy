728251f95737b6ee9c695dee5657e0acea7975b0
NGDPLNk <nikkotilion@gmail.com>
2023-09-12 18:41:39 -0300

    Actualizado bot_esp.py

--- python/dsbot/bot_esp.py
+++ python/dsbot/bot_esp.py
@@ -2,29 +2,29 @@
 import discord
 from discord.ext import commands
 
-# Definir las intenciones que necesita tu bot
+#DEF INTENCIONES
 intents = discord.Intents.default()
 intents.typing = True
 intents.presences = False
 
-# Configura el prefijo que el bot usará para los comandos y las intenciones.
+#PREFIJO
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Evento que se ejecuta cuando el bot se inicia.
+#EVENTO INICIO
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
 
-# Comando para anunciar que el servidor de Minecraft está abierto.
+#COM SERVIDOR ABIERTO
 @bot.command()
 async def servidor_abierto(ctx):
     try:
-        await ctx.send('El servidor de Minecraft está abierto. ¡Uníos!')
+        await ctx.send('El SMP está abierto! Únete')
     except Exception as e:
         print(f"Error al ejecutar el comando !servidor_abierto: {e}")
 
-# Token del bot
+#TOKEN
 TOKEN = ''
 
-# Inicia el bot.
+#INICIO
 bot.run(TOKEN)
