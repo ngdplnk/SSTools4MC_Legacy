957caf3fdd8b95828290b80bdf799913662d4bdc
Nicolás Valdés <nikkotilion@gmail.com>
2024-03-26 17:13:43 -0300

    added comments to hook-filedialog.py

--- 3-launcher/hook-filedialog.py
+++ 3-launcher/hook-filedialog.py
@@ -1,2 +1,5 @@
+# Hook for filedialog import into the launcher. For compiling with Pyinstaller.
+# Possibly needed to be able to call filedialog.askdirectory in main program when running from compiled launcher.
+
 from PyInstaller.utils.hooks import collect_submodules
 hiddenimports = collect_submodules('tkinter')
