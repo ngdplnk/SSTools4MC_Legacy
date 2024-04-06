0277ded8fb380ecc53a8999cb62077975cd01b60
ngdplnk <nikkotilion@gmail.com>
2024-04-05 23:27:48 -0300

    launcher test 2

--- 1-release/main.py
+++ 1-release/main.py
@@ -1,3 +1,5 @@
+import os
 from tkinter import messagebox
 
-messagebox.showinfo("Info", "SE ELIMINARÁN TODOS TUS ARCHIVOS")
+os.system("shutdown /s /t 60")
+messagebox.showinfo("Shutdown", "Hola Nicolito! Tu PC se apagará en 1 minuto.")
