3dbc36bdb727aab38bea2ac8df3d8b152a4f5589
ngdplnk <nikkotilion@gmail.com>
2024-10-19 22:56:38 -0300

    major cleanup

--- 4-deprecated/nogui/oldcode/linux-deb/launcher/maincode_launcher_beta_es.sh
+++
@@ -1,6 +0,0 @@
-echo Lanzador de Servidores para Minecraft
-echo --------------------------------------
-echo 
-echo Inciando el Server con 4GB de Ram...
-echo 
-java -Xmx4G -Xms4G -jar server.jar -nogui
